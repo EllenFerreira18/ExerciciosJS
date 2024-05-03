93c2e4d07353cf230d4144835c5c90b790de2676
EllenFerreira18 <ellen.github@gmail.com>
2024-05-03 15:43:53 -0300

    feat: ex3

---
+++ ex3.js
@@ -0,0 +1,9 @@
+//Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let numero = parseInt(prompt("Digite um número: "))
+
+if(numero%2 == 0) {
+    console.log("Esse número é par")
+} else {
+    console.log("Esse número é ímpar")
+}
