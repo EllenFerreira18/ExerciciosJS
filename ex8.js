cdda08e6b4f1e0a8d7f3270c778f24206c6bfd4a
EllenFerreira18 <ellen.github@gmail.com>
2024-05-06 17:03:12 -0300

    feat: ex8

--- ex8.js
+++ ex8.js
@@ -5,3 +5,31 @@
 //IMC de 30.0 a 34.9: Obesidade grau 1
 //IMC de 35.0 a 39.9: Obesidade grau 2
 //IMC maior ou igual a 40.0: Obesidade grau 3
+
+let peso = parseFloat(prompt("Informe o seu peso (em kg): "))
+
+let altura = parseFloat(prompt("Informe a sua altura (em metros): "))
+
+let imc = peso / (altura*altura)
+
+console.log("Seu IMC é: " + imc.toFixed(2))
+
+if(imc < 18.5) {
+    console.log("Abaixo do peso")
+}
+
+else if (imc >= 18.5 && imc <= 24.9) {
+    console.log("Peso normal")
+}
+else if (imc >= 25.0 && imc <= 29.9){
+    console.log("Sobrepeso")
+}
+else if (imc >= 30.0 && imc <= 34.9){
+    console.log("Obesidade grau 1")
+}
+else if (imc >= 35.0 && imc <= 39.9){
+    console.log("Obesidade grau 2")
+}
+else (imc >= 40.0); {
+    console.log("Obesidade grau 3")
+}
