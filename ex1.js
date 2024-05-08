8edadae58050ce0d6905adbe5e0891b89fa45af0
Ellen Ferreira <166646742+EllenFerreira18@users.noreply.github.com>
2024-05-08 15:02:14 -0300

    Delete ex1.js

--- ex1.js
+++
@@ -1,8 +0,0 @@
-let username = prompt("Digite seu nome de usuário: ")
-let senha = prompt ("Digite sua senha: ")
-
-if(username == "admin" && senha == "senha123") {
-    console.log("Login bem sucedido");
-} else {
-    console.log("Você não tem permissão de acesso!")
-}
