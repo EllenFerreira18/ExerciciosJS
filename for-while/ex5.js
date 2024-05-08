23f63ff7fd47eac2ae96d5b72d967be8272e3fb7
EllenFerreira18 <ellen.github@gmail.com>
2024-05-08 15:49:04 -0300

    feat: ex6

---
+++ for-while/ex5.js
@@ -0,0 +1,11 @@
+let n1 = 1
+let n2 = 0
+let n3 = 0
+
+for(i=1; i<=10; i++){
+    console.log(n1)
+
+    n3 = n1 +n2
+    n2 = n1
+    n1 = n3
+}
---
+++ for-while/ex6.js
@@ -0,0 +1,10 @@
+let numeros = 0 
+
+while (numeros <= 20){
+    if(numeros%2 == 0) {
+        console.log(numeros)
+    }   
+
+    numeros++
+}
+
