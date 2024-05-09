92c080db149fd3077c9f91dea536bca5c115a7d7
EllenFerreira18 <ellen.github@gmail.com>
2024-05-09 14:47:23 -0300

    feat: ex9

---
+++ for-while/ex9.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("Digite um número: "));
+
+console.log("Contagem regressiva está começando em: " + numero)
+
+while (numero >= 0){
+    console.log(numero);
+    numero--
+}
